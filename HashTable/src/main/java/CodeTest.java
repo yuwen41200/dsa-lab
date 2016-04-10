9a22e8f8a4612af20c26c877fcad661b615f4219
yuwen41200 <yuwen41200@gmail.com>
2016-04-11 03:09:49 +0800

    Finished but not tested yet.

--- HashTable/src/main/java/CodeTest.java
+++ HashTable/src/main/java/CodeTest.java
@@ -1,5 +1,5 @@
 /**
- * Homework #2 Sort (Binary Input)
+ * Homework #3 Same Pattern
  * Unit Test
  */
 
@@ -9,77 +9,39 @@ import org.msgpack.core.MessageUnpacker;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.lang.Thread;
-import java.util.Arrays;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class CodeTest {
 
 public static void main(String[] args) throws Exception {
 	// preparing inputs
-	final int dataSize1 = 1;
-	final int dataSize2 = 25;
-	final int dataSize3 = 50;
-	final int dataSize4 = 75;
-	final int dataSize5 = 100;
-	int[] dataSet1 = new int[dataSize1];
-	int[] dataSet2 = new int[dataSize2];
-	int[] dataSet3 = new int[dataSize3];
-	int[] dataSet4 = new int[dataSize4];
-	int[] dataSet5 = new int[dataSize5];
+	final int count = 2;
+	final String pattern1 = "xyxy";
+	final String sequence1 = "sense lab sense lab";
+	final int match1 = 1;
+	final String pattern2 = "xyxy";
+	final String sequence2 = "sense lab lab sense";
+	final int match2 = 0;
 	// generating inputs
-	for (int i = 0; i < dataSize1; i++)
-		dataSet1[i] = ThreadLocalRandom.current().nextInt();
-	for (int i = 0; i < dataSize2; i++)
-		dataSet2[i] = ThreadLocalRandom.current().nextInt();
-	for (int i = 0; i < dataSize3; i++)
-		dataSet3[i] = ThreadLocalRandom.current().nextInt();
-	for (int i = 0; i < dataSize4; i++)
-		dataSet4[i] = ThreadLocalRandom.current().nextInt();
-	for (int i = 0; i < dataSize5; i++)
-		dataSet5[i] = ThreadLocalRandom.current().nextInt();
+	String[] seqArray1 = sequence1.split(" ");
+	String[] seqArray2 = sequence2.split(" ");
 	// writing inputs
 	MessagePacker packer = MessagePack.newDefaultPacker(new FileOutputStream("input.txt"));
-	packer.packInt(5);
-	packer.packArrayHeader(dataSize1);
-	for (int i = 0; i < dataSize1; i++)
-		packer.packInt(dataSet1[i]);
-	packer.packArrayHeader(dataSize2);
-	for (int i = 0; i < dataSize2; i++)
-		packer.packInt(dataSet2[i]);
-	packer.packArrayHeader(dataSize3);
-	for (int i = 0; i < dataSize3; i++)
-		packer.packInt(dataSet3[i]);
-	packer.packArrayHeader(dataSize4);
-	for (int i = 0; i < dataSize4; i++)
-		packer.packInt(dataSet4[i]);
-	packer.packArrayHeader(dataSize5);
-	for (int i = 0; i < dataSize5; i++)
-		packer.packInt(dataSet5[i]);
+	packer.packInt(count);
+	packer.packString(pattern1);
+	packer.packArrayHeader(seqArray1.length);
+	for (String word : seqArray1)
+		packer.packString(word);
+	packer.packString(pattern2);
+	packer.packArrayHeader(seqArray2.length);
+	for (String word : seqArray2)
+		packer.packString(word);
 	packer.close();
 	// running the tests
 	Code.main(null);
-	Arrays.sort(dataSet1);
-	Arrays.sort(dataSet2);
-	Arrays.sort(dataSet3);
-	Arrays.sort(dataSet4);
-	Arrays.sort(dataSet5);
 	// verifying outputs
 	MessageUnpacker unpacker = MessagePack.newDefaultUnpacker(new FileInputStream("output.txt"));
-	assertEquals(dataSize1, unpacker.unpackArrayHeader());
-	for (int i = 0; i < dataSize1; i++)
-		assertEquals(dataSet1[i], unpacker.unpackInt());
-	assertEquals(dataSize2, unpacker.unpackArrayHeader());
-	for (int i = 0; i < dataSize2; i++)
-		assertEquals(dataSet2[i], unpacker.unpackInt());
-	assertEquals(dataSize3, unpacker.unpackArrayHeader());
-	for (int i = 0; i < dataSize3; i++)
-		assertEquals(dataSet3[i], unpacker.unpackInt());
-	assertEquals(dataSize4, unpacker.unpackArrayHeader());
-	for (int i = 0; i < dataSize4; i++)
-		assertEquals(dataSet4[i], unpacker.unpackInt());
-	assertEquals(dataSize5, unpacker.unpackArrayHeader());
-	for (int i = 0; i < dataSize5; i++)
-		assertEquals(dataSet5[i], unpacker.unpackInt());
+	assertEquals(match1, unpacker.unpackInt());
+	assertEquals(match2, unpacker.unpackInt());
 	unpacker.close();
 	// exiting the tests
 	System.exit(0);
