e97fa33b15df8a6caab074ba0173a369f7a53fdc
yuwen41200 <yuwen41200@gmail.com>
2016-06-20 20:33:30 +0800

    Working on HW6.

--- greedy-algorithm/main.cpp
+++ greedy-algorithm/main.cpp
@@ -1,8 +1,37 @@
-#include <iostream>
+/**
+ * Introduction to Algorithms 2016 Spring
+ * Homework #6 Minimal Cover
+ * by Yu-wen Pwu (rev1)
+ */
 
-using namespace std;
+#include <cmath>
+#include <cstdio>
+#include <utility>
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+	FILE *input = fopen("input.txt", "r");
+	FILE *output = fopen("output.txt", "w");
+	if (!input || !output) {
+		perror("cannot open files");
+		return 1;
+	}
+
+	int circle, length, width;
+	while (fscanf(input, "%d %d %d", &circle, &length, &width) == 3) {
+		std::pair<float, float> *range = new std::pair<float, float>[circle];
+		for (int i = 0; i < circle; ++i) {
+			int position, radius;
+			fscanf(input, "%d %d", &position, &radius);
+			if (radius <= width / 2.0f)
+				continue;
+			float project = sqrtf(powf(radius, 2) - powf(width / 2.0f, 2));
+			range[i] = std::make_pair(position - project, position + project);
+		}
+
+		// TODO: sort range by its second element
+	}
+
+	fclose(input);
+	fclose(output);
+	return 0;
 }
